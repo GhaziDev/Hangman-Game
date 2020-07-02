be8813269df4cbc5629d0b472938f6d461f2fccb
ghazigamer <46258765+ghazigamer@users.noreply.github.com>
2020-07-03 07:51:58 +1200

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,52 @@
+import random
+f=open('wordgenerator.txt','r')
+words_list=[i for i in f.read().splitlines()]
+
+
+class Hangman:
+    def __init__(self):
+        self.word=random.choice(words_list)
+        self.guess='-'*len(self.word)
+        self.lives=8
+        self.occurences=[]
+    def play(self):
+        wordlist=list(self.word)
+        
+        while(self.lives>0):
+            print(self.guess)
+            ask=input("Guess the correct letter of this word :  ").lower()
+            if ask in self.occurences:
+                print("\nYou have already guessed that letter")
+            
+            
+            elif ask not in  wordlist:
+                self.lives-=1
+                self.occurences.append(ask)
+                print(f"\nWrong guess,you have {self.lives} lives left")
+                if(self.lives==0):
+                    print("\nyou lost")
+                    print(f"\nthe word is : {self.word}")
+            else:
+                self.guess=list(self.guess)
+                i=0
+                while(i<len(self.word)):
+                    if ask==wordlist[i]:
+                        self.guess[i]=ask
+                    i+=1
+                self.guess=''.join(self.guess)
+                self.occurences.append(ask)
+                if('-' not in self.guess):
+                    print(f"\nThe word is : {self.guess}")
+                    print("\nYou have passed the trickster!")
+                    break
+            
+        del self.occurences
+h=Hangman()
+h.play()
+#O(n^2) worst case
+#o(n) best case
+            
+
+
+
+
